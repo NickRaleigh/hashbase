4be16b0cfbfd433d85d4dc3a779d07905a2611c1
Nick Raleigh <hello@nickraleigh.com>
2017-12-26 00:38:11 -0700

    fixes array printing issue

--- hash/class/write.rb
+++ hash/class/write.rb
@@ -9,9 +9,21 @@ module Write
         open(hashLocation, 'a') { |f| f.puts  "$#{hashID} = {\n"}
         @select.each do |k, v|
           if i > 1 
-            open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>\"#{v}\",\n"}
+            if v.kind_of?(String)
+              puts "#{v} it's a string"
+              open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>\"#{v}\",\n"}
+            elsif v.kind_of?(Array)
+              puts "#{v} it's an array"
+              open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>#{v},\n"}
+            end
           elsif i == 1
-            open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>\"#{v}\"\n"}
+            if v.kind_of?(String)
+              puts "#{v} it's a string"
+              open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>\"#{v}\"\n"}
+            elsif v.kind_of?(Array)
+              puts "#{v} it's an array"
+              open(hashLocation, 'a') { |f| f.puts  "\t\"#{k}\"=>#{v}\n"}
+            end
           end
         i = i - 1
       end
